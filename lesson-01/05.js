be454992b472a712ef7843f9345c416d64944956
vlmna13 <vlmna13@gmail.com>
2024-06-05 12:29:55 +0300

    r

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -5,9 +5,7 @@ switch (true) {
     case rule.length > 25: 
      console.log("У меня 100 пудов все получится");
      break;
-    case rule.length === 25:
-     console.log("50 на 50");
-     break;
      default:
+      console.log("50 на 50");
       break;
    }
