a0d7ecffff60d38d4560ef690000582a17545c3f
vlmna13 <vlmna13@gmail.com>
2024-06-05 11:58:30 +0300

    rafactor 5

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -4,9 +4,9 @@ switch (true) {
      console.log( "Все таки нет правил без исключения"); 
      break; 
     case rule.length > 25: 
-     console.log("У меня 100 пудов все получится");
+     console.log( "У меня 100 пудов все получится");
      break;
     case rule.length === 25:
-     console.log("50 на 50")
+     console.log( "50 на 50")
      break;
    }
